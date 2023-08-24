1ca4e340c513a71df345e271ff2654e42f98d7b9
github_fedeesti <fede.estigarribia@hotmail.com>
2023-08-24 15:18:58 -0300

    Add Home page

---
+++ src/pages/Home.tsx
@@ -0,0 +1,26 @@
+function Home() {
+  return (
+    <div className="relative py-16">
+      <div className="relative container m-auto px-6 md:px-12 xl:px-40" data-cy="home-container">
+        <div className="m-auto md:w-8/12 lg:w-6/12 xl:w-6/12">
+          <div className="p-6 sm:p-16">
+            <h2 className="mb-8 text-2xl font-bold text-center" data-cy="home-title">
+              Connect with a wallet
+            </h2>
+            <div className="mt-16 grid space-y-4" data-cy="home-button-list-container">
+              <button className="group h-12 px-6" data-cy="home-connect-secret-key">
+                <div className="relative flex items-center space-x-4 justify-center">
+                  <span className="block w-max font-semibold tracking-wide text-gray-700 text-sm sm:text-base underline">
+                    Connect with a secret key
+                  </span>
+                </div>
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Home;
