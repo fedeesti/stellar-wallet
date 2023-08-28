df3d326c8a4abca18ba43e0e6ab75306ad26645b
github_fedeesti <fede.estigarribia@hotmail.com>
2023-08-28 12:44:54 -0300

    Fix styles on DashboardWallet

--- src/pages/DashboardWallet.tsx
+++ src/pages/DashboardWallet.tsx
@@ -3,7 +3,7 @@ import BalanceInfo from '../components/dashboard/BalanceInfo';
 function DashboardWallet() {
   return (
     <main className="text-stellar-ghostwhite" data-cy="dashboard-main-container">
-      <section className="my-8 px-8 bg-[#303448]" data-cy="dashboard-balance-section-container">
+      <section className="my-8 p-8 bg-[#303448]" data-cy="dashboard-balance-section-container">
         <div className="px-12 my-o mx-auto max-w-7xl w-full">
           <BalanceInfo />
         </div>
