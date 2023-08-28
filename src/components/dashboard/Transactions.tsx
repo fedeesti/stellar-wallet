a97ceaca789fe2e45f5e745fd31cf099321aa4c5
github_fedeesti <fede.estigarribia@hotmail.com>
2023-08-28 14:25:36 -0300

    Add Transaction component

---
+++ src/components/dashboard/Transactions.tsx
@@ -0,0 +1,12 @@
+function Transactions() {
+  return (
+    <section className="bg-[#292d3e] px-8 mb-12">
+      <div className="mb-8">
+        <h3 className="mb-4 font-medium text-4xl">Liquidity Pool Transactions</h3>
+        <p className="text-sm mb-4">There are no recent liquidity pool transactions to show</p>
+      </div>
+    </section>
+  );
+}
+
+export default Transactions;
--- src/pages/DashboardWallet.tsx
+++ src/pages/DashboardWallet.tsx
@@ -1,13 +1,15 @@
 import BalanceInfo from '../components/dashboard/BalanceInfo';
 import PaymentsHistory from '../components/dashboard/PaymentsHistory';
+import Transactions from '../components/dashboard/Transactions';
 
 function DashboardWallet() {
   return (
-    <main className="text-stellar-ghostwhite" data-cy="dashboard-main-container">
+    <main className="text-stellar-ghostwhite bg-[#292d3e]" data-cy="dashboard-main-container">
       <section className="my-8 p-8 bg-[#303448]" data-cy="dashboard-balance-section-container">
         <BalanceInfo />
       </section>
       <PaymentsHistory />
+      <Transactions />
     </main>
   );
 }
