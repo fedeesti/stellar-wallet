ce8a25d562c623a185f45a13ccd9a86472efec70
github_fedeesti <fede.estigarribia@hotmail.com>
2023-08-30 15:54:24 -0300

    Add account viewer button

--- src/components/navbar/NavBar.tsx
+++ src/components/navbar/NavBar.tsx
@@ -3,9 +3,17 @@ import stellarLogo from '../../assets/stellar-logo.svg';
 function NavBar() {
   return (
     <nav className="bg-white border-gray-200 max-w-screen-xl" data-cy="nav-container">
-      <a href="https://www.stellar.org/" className="flex items-center" data-cy="nav-logo-link">
-        <img src={stellarLogo} className="h-24 ml-4" alt="Stellar Logo" data-cy="nav-logo" />
-      </a>
+      <div className="flex items-center gap-2">
+        <a href="https://www.stellar.org/" className="flex items-center" data-cy="nav-logo-link">
+          <img src={stellarLogo} className="h-24 ml-4" alt="Stellar Logo" data-cy="nav-logo" />
+        </a>
+        <div
+          className="bg-[#5332e6] text-[#ffffff] uppercase text-sm font-medium py-0.2 px-1.5 rounded"
+          data-cy="nav-btn-account-viewer"
+        >
+          Account Viewer
+        </div>
+      </div>
     </nav>
   );
 }
