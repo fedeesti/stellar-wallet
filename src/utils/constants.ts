6331522f72640cb2efc3c87b1d38e6cc3802072d
github_fedeesti <fede.estigarribia@hotmail.com>
2023-08-30 11:22:06 -0300

    Fix revision changes in SecretKeyLogin and create a constants file

---
+++ src/utils/constants.ts
@@ -0,0 +1,11 @@
+export const errorExceptions = {
+  empty: 'empty',
+  valid: 'valid',
+  invalid: 'invalid',
+};
+
+export const errorPrivateKeyMessage = {
+  empty: '',
+  withoutPrivateKey: 'Please enter your secret key',
+  invalid: 'Invalid secret key. Secret keys are uppercase and begin with the letter "S."',
+};
