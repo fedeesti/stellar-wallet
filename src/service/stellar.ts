0b79da5b95aa6fa4417a7c0405b83cf0e1713874
github_fedeesti <fede.estigarribia@hotmail.com>
2023-09-06 10:41:59 -0300

    Add loadTransactionHistory service

--- src/service/stellar.ts
+++ src/service/stellar.ts
@@ -1,4 +1,7 @@
-import { Keypair } from 'stellar-sdk';
+import { Keypair, Server } from 'stellar-sdk';
+import { ServerApi } from 'stellar-sdk/lib/server_api';
+
+const server = new Server(import.meta.env.VITE_URL_HORIZON);
 
 export function getPublicKeyFromPrivateKey(privateKey: string): string {
   try {
@@ -6,5 +9,24 @@ export function getPublicKeyFromPrivateKey(privateKey: string): string {
     return keyPair.publicKey();
   } catch (e) {
     throw new Error('The private key was invalid');
+  }
+}
+
+export async function loadTransactionHistory(
+  signerAccountPublicKey: string,
+): Promise<ServerApi.OperationRecord[]> {
+  try {
+    const transactionLimit = 100;
+
+    const transaction: ServerApi.CollectionPage<ServerApi.PaymentOperationRecord> = await server
+      .payments()
+      .forAccount(signerAccountPublicKey)
+      .order('desc')
+      .limit(transactionLimit)
+      .call();
+
+    return transaction.records;
+  } catch (error) {
+    throw new Error('Could not find transaction history');
   }
 }
