fb45ab96b690b003865e70e7461f70d320fdbdb3
github_fedeesti <fede.estigarribia@hotmail.com>
2023-09-06 15:34:33 -0300

    Add AuthPublicKeyProvider inside the App component

--- src/App.tsx
+++ src/App.tsx
@@ -3,17 +3,18 @@ import Footer from './components/footer/Footer';
 import NavBar from './components/navbar/NavBar';
 import DashboardWallet from './pages/DashboardWallet';
 import Home from './pages/Home';
+import AuthPublicKeyProvider from './contexts/PublicKeyContext';
 
 function App() {
   return (
-    <>
+    <AuthPublicKeyProvider>
       <NavBar />
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="" element={<DashboardWallet />} />
+        <Route path="/dashboard" element={<DashboardWallet />} />
       </Routes>
       <Footer />
-    </>
+    </AuthPublicKeyProvider>
   );
 }
 
