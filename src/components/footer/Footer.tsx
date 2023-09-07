757f657250f22b52065b2759b7f0df374800f6f7
github_fedeesti <fede.estigarribia@hotmail.com>
2023-09-07 11:17:27 -0300

    Fix property DOM and colors on Footer

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -1,8 +1,8 @@
 function Footer() {
   return (
-    <footer className="px-12 bg-[#3a3e4d] text-[#ffffff]" data-cy="footer-container">
-      <div className="w-full max-w-screen-xl pb-12 pt-6 border-t border-[#4b4f5d] md:flex md:items-center md:justify-between">
-        <ul className="flex flex-wrap items-center mt-3 font-bold hover:text-[#d4d5d8] dark:text-gray-400 sm:mt-0">
+    <footer className="px-12 bg-stellar-border-primary text-white" data-cy="footer-container">
+      <div className="w-full max-w-screen-xl pb-12 pt-6 border-t border-stellar-border-secondary md:flex md:items-center md:justify-between">
+        <ul className="flex flex-wrap items-center mt-3 font-bold hover:text-stellar-text-secondary dark:text-gray-400 sm:mt-0">
           <li data-cy="footer-terms-of-service-title">
             <a
               href="https://www.stellar.org/terms-of-service"
@@ -27,7 +27,7 @@ function Footer() {
         <div>
           <a
             href="https://github.com/fedeesti/stellar-wallet"
-            className="ml-2 flex items-center gap-1 font-bold hover:text-[#d4d5d8]"
+            className="ml-2 flex items-center gap-1 font-bold hover:text-stellar-text-secondary"
             target="_blank"
             data-cy="footer-repository-link"
           >
@@ -38,9 +38,9 @@ function Footer() {
               viewBox="0 0 24 24"
               fill="none"
               stroke="currentColor"
-              stroke-width="2"
-              stroke-linecap="round"
-              stroke-linejoin="round"
+              strokeWidth="2"
+              strokeLinecap="round"
+              strokeLinejoin="round"
               className="w-5 h-5"
             >
               <path d="M9 19c-5 1.5-5-2.5-7-3m14 6v-3.87a3.37 3.37 0 00-.94-2.61c3.14-.35 6.44-1.54 6.44-7A5.44 5.44 0 0020 4.77 5.07 5.07 0 0019.91 1S18.73.65 16 2.48a13.38 13.38 0 00-7 0C6.27.65 5.09 1 5.09 1A5.07 5.07 0 005 4.77a5.44 5.44 0 00-1.5 3.78c0 5.42 3.3 6.61 6.44 7A3.37 3.37 0 009 18.13V22"></path>
