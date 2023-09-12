f22d13d32435a7fc777a0d5a210567f7b8d3a49e
github_fedeesti <fede.estigarribia@hotmail.com>
2023-09-12 12:55:58 -0300

    Add localSotrage and remove reload on payments.ts

--- src/service/payments.ts
+++ src/service/payments.ts
@@ -7,9 +7,9 @@ import {
   Server,
   TransactionBuilder,
 } from 'stellar-sdk';
+import { decrypt } from './security/security';
 
 interface ISendPayment {
-  signerAccountKeypair: Keypair;
   signerAccountPublicKey: string;
   destinationId: string;
   amount: string;
@@ -17,13 +17,15 @@ interface ISendPayment {
 
 const server = new Server(import.meta.env.VITE_URL_HORIZON);
 
-export async function sendPayments({
-  signerAccountKeypair,
+export async function sendPaymentWithSecretKey({
   signerAccountPublicKey,
   destinationId,
   amount,
 }: ISendPayment) {
   try {
+    const privateKey = decrypt(localStorage.getItem('secret') as string);
+    const signerKeypair = Keypair.fromSecret(privateKey);
+
     const signerAccount = await server.loadAccount(signerAccountPublicKey);
 
     const transaction = new TransactionBuilder(signerAccount, {
@@ -40,10 +42,9 @@ export async function sendPayments({
       .setTimeout(180)
       .build();
 
-    transaction.sign(signerAccountKeypair);
+    transaction.sign(signerKeypair);
 
-    await server.submitTransaction(transaction);
-    location.reload();
+    return await server.submitTransaction(transaction);
   } catch (error) {
     throw new Error('Failed to send transaction');
   }
