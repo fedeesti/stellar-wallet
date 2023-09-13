05d19a770d2ba7b00465f28702a4df247ccd43fe
github_fedeesti <fede.estigarribia@hotmail.com>
2023-09-13 10:54:12 -0300

    Add balance on DashboardWallet

--- src/pages/DashboardWallet.tsx
+++ src/pages/DashboardWallet.tsx
@@ -1,15 +1,18 @@
 import Balance from '../components/dashboard/Balance';
 import TransactionHistory from '../components/dashboard/TransactionHistory';
 import useAuthPublicKey from '../hooks/useAuthPublicKey';
+import useLoadAccount from '../hooks/useLoadAccount';
 
 function DashboardWallet() {
   const { publicKey } = useAuthPublicKey();
+  const { balance } = useLoadAccount(publicKey as string);
+
   return (
     <main
       className="text-stellar-text-primary bg-stellar-bg-primary"
       data-cy="dashboard-main-container"
     >
-      <Balance publicKey={publicKey as string} />
+      <Balance publicKey={publicKey as string} balance={balance as string} />
       <TransactionHistory />
     </main>
   );
